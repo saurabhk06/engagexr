accdb59fbbfc23ae359b37fb38f93baac45b9c80
Saurabh Kumar <sk702199@gmail.com>
2022-08-16 16:25:30 +0530

    config file udpated

--- src/configs/sequelizeconfig.ts
+++ src/configs/sequelizeconfig.ts
@@ -4,10 +4,10 @@ import { Sequelize } from 'sequelize-typescript';
  * This method generates the sequelize instance to use.
  */
 export const sequelize = new Sequelize({
-  database: 'engagexrtestdb',
+  database: 'YOUR_DATABASE_NAME',
   dialect: 'mysql',
-  username: 'root',
-  password: 'damco06',
+  username: 'YOUR_USERNAME',
+  password: 'YOUR_PASSWORD',
   storage: ':memory:',
   models: [__dirname + '/../models'],
 });
